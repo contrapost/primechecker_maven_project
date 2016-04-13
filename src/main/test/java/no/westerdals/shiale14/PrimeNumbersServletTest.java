d004f2dcc1f4ba830d94650e532999002a117e34
Alexander Shipunov <shiale14@student.westerdals.no>
2016-04-13 22:21:46 +0200

    add test class + change buffer for logs

--- src/main/test/java/no/westerdals/shiale14/PrimeNumbersServletTest.java
+++
@@ -1,8 +0,0 @@
-package no.westerdals.shiale14;
-
-/**
- *
- * Created by Alexander Shipunov on 11.04.2016.
- */
-public class PrimeNumbersServletTest {
-}
---
+++ src/test/java/no/westerdals/shilae14/PrimeNumbersServletTest.java
@@ -0,0 +1,16 @@
+package java.no.westerdals.shilae14;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ *
+ * Created by Alexander Shipunov on 13.04.2016.
+ */
+public class PrimeNumbersServletTest {
+
+    @Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
